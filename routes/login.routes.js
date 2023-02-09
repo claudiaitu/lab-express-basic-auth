f64cca982ba8bb47e318a31c892f621888b60277
Claudia Iturregui <claudia.iturregui@gmail.com>
2023-02-09 15:37:58 -0500

    finito

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
-const User = require('../models/User.model')
+const User = require('../models/User.model');
+
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/',isLoggedOut, (req, res, next) => {
   res.render('login/user-login');
 });
 
-router.post('/', (req, res, next) => {
+router.post('/',isLoggedOut, (req, res, next) => {
   const {username, password} = req.body;
 
   bcryptjs
@@ -27,14 +29,48 @@ router.post('/', (req, res, next) => {
   })
   .then((createdUser) => {
     console.log(createdUser);
-    res.send('Login successful')
+    res.redirect('/login/userLogin')
   })
   .catch((err) => {
     console.log(err)
   })
-
-  
 })
+
+router.get('/userLogin',isLoggedOut, (req, res, next) => {
+  res.render('login/user-real-login.hbs')
+})
+
+router.post('/userLogin',isLoggedOut, (req, res, next) => {
+  const {username, password} = req.body
+
+  if(!username || !password) {
+    res.render('login/user-real-login', { errorMessage: "Please enter both username and password to login."})
+    return;
+  }
+
+  User.findOne({username})
+  .then((user => {
+    if(!user) {
+      res.render('Username is not registered. Try with other username.')
+      return;
+    } else if (bcryptjs.compareSync(password, user.password)) {
+      req.session.user = user;
+      console.log('SESSION =====> ', req.session);
+    res.redirect('/login/userProfile')
+    } else {
+    res.render('login/user-real-login.hbs', { errorMessage: 'Password incorrect, Try again.'})
+    }
+  }))
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.get('/userProfile', isLoggedIn, (req, res, next) => {
+  const user = req.session.user;
+  res.render('login/user-profile.hbs', {user})
+})
+
 
 
 module.exports = router;
